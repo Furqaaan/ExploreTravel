bd88e11ca0b09b2833d7c8fbd0b291d32e80945f
Furqaaan <furqancode@gmail.com>
2022-08-10 23:43:38 +0530

    Fixed selection of elements to use class names instead of selecting by elements directly

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,13 +4,13 @@ function Header() {
 	return (
 		<div className="header-container">
 			<img src="images/logo.png" alt="explore travel logo" />
-			<ul>
-				<li className="active"><a href="#">Home</a></li>
-				<li><a href="#">Destinations</a></li>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Partner</a></li>
-				<li className='secondary-btn'><a href="#">Login</a></li>
-				<li className='primary-btn'><a href="#">Register</a></li>
+			<ul className="header-container-list">
+				<li className="active"><a className="header-container-links" href="#">Home</a></li>
+				<li><a className="header-container-links" href="#">Destinations</a></li>
+				<li><a className="header-container-links" href="#">About</a></li>
+				<li><a className="header-container-links" href="#">Partner</a></li>
+				<li className='secondary-btn'><a className='btn-link' href="#">Login</a></li>
+				<li className='primary-btn'><a className='btn-link' href="#">Register</a></li>
 			</ul>
 		</div>
 	);
