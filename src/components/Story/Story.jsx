ba8ab7bb48d38dc72984086d1ed0e06943d8a781
Furqaaan <furqancode@gmail.com>
2022-08-17 09:27:45 +0530

    Changed to use classname selectors instead of element selectors

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -3,10 +3,10 @@ import "./Story.css"
 function Story(props) {
   return (
 	<div className="trending-story-item">
-		<img src={`images/trending/${props.story.image}`} alt='trending stories' />
-		<h4>{props.story.title}</h4>
-		<p>{props.story.description}</p>
-		<a href="#">Read more</a>
+		<img className="trending-story-image" src={`images/trending/${props.story.image}`} alt='trending stories' />
+		<h4 className="trending-story-title">{props.story.title}</h4>
+		<p className="trending-story-content">{props.story.description}</p>
+		<a className="trending-story-read-more" href="#">Read more</a>
 	</div>
   )
 }
