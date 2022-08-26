778dea1cb5142cc796163dce881474ac8b978404
Furqaaan <furqancode@gmail.com>
2022-08-26 09:31:48 +0530

    Removed unwanted css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 import Testimonials from './components/Testimonials/Testimonials'
 
 function App() {
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import "./Testimonials.css"
+import "./Testimonials.scss"
 
 function Testimonials() {
 
